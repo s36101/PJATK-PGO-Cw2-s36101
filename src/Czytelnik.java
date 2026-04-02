9bbdd62fc73b536b6c4bb155f842d8b7de3b8edf
Artur Tomala <s36101@pjwstk.edu.pl>
2026-04-02 22:45:13 +0200

    Dodanie klasy Czytelnik i metod

---
+++ src/Czytelnik.java
@@ -0,0 +1,31 @@
+public class Czytelnik {
+    private String imie;
+    private String nazwisko;
+    private int numerKarty;
+    private int liczbaWypozyczen;
+
+    public Czytelnik(String imie, String nazwisko, int numerKarty) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numerKarty = numerKarty;
+        this.liczbaWypozyczen = 0;
+    }
+
+    public String wypiszDane() {
+        System.out.println("Czytelnik: " + imie + " " + nazwisko " (Karta: " + numerKarty + ") Wypożyczone: " + liczbaWypozyczen);
+    }
+
+    public void zwiekszLiczbeWypozyczen() {
+        liczbaWypozyczen++;
+    }
+
+    public void zmniejszLiczbeWypozyczen() {
+        if (liczbaWypozyczen > 0) {
+            liczbaWypozyczen--;
+        }
+    }
+
+    public String getImie() {
+        return imie;
+    }
+}
